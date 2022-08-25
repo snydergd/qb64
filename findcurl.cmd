786c050bbffe64a36a797e262974b57367db9ed8
brickviking <brickviking@gmail.com>
2022-08-26 02:00:21 +1200

    Finally got the ARCH stuff working
    Arch discovery needed to be outside the test for errorlevel

--- findcurl.cmd
+++ findcurl.cmd
@@ -8,17 +8,17 @@ rem It also assumes that curl is downloaded to the users Desktop
 set DLPAGE=http://skanthak.homepage.t-online.de/download
 set CURLVERSION=curl-7.64.1.cab
 set LINK=%DLPAGE%/%CURLVERSION%
-rem Check if curl exists
+rem grab the arch. We'll need this to extract files.
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set ARCH=i386|| set ARCH=amd64
 
+rem Check if curl exists
 curl --version 2>NUL 1>&2
+
 
 if %ERRORLEVEL == 9009 (
     mkdir internal\curl >NUL
     echo Fetching %LINK%
     explorer %LINK%
-
-    rem grab the arch. We'll need this to extract files.
-    reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set ARCH=i386|| set ARCH=amd64
 
     rem we should wait until the file is downloaded, because explorer returns straight away
     "%SystemRoot%\system32\expand.exe" "%USERPROFILE%\Desktop\%CURLVERSION%" /F:%ARCH%\* internal\curl
@@ -28,4 +28,3 @@ if %ERRORLEVEL == 9009 (
 ) ELSE (
     echo "Found curl, continuing"
 )
-
